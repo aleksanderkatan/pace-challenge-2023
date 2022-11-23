c827a8f1cb5eeef39a96f86e6231cc75d5c4383d
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-11-23 20:33:21 +0100

    Add half of the constraints to the encoder

--- algorithms/relative_encoding_algorithm.py
+++ algorithms/relative_encoding_algorithm.py
@@ -1,5 +1,6 @@
 from pysat.solvers import Solver
 from pysat.formula import IDPool
+from pysat.formula import CNFPlus
 import networkx as nx
 
 
@@ -46,8 +47,71 @@ class Encoder:
             return self.v["a", i, j]
         return -self.v["a", j, i]
 
+    # implies([p_1, p_2, p_3], q) returns a clause equivalent to
+    # p_1 && p_2 && p_3 => q
+    def implies(self, p, q):
+        return [-p_i for p_i in p] + [q]
+
     def encode(self):
-        return None
+        formula = CNFPlus()
+        n = len(self.g.nodes)
+
+        # ordering
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                for k in range(1, n+1):
+                    # if all are different
+                    if len({i, j, k}) == 3:
+                        clause = self.implies(
+                            [self.o(i, j), self.o(j, k)],
+                            self.o(i, k)
+                        )
+                        formula.append(clause)
+
+        # parents are SMALLER than their children
+        for i in range(1, n+1):
+            for j in range(i+1, n+1):
+                clause = self.implies(
+                    [self.p(i, j)],
+                    self.o(i, j)
+                )
+                formula.append(clause)
+
+        # every vertex (except n) is a parent to at least one vertex
+        for i in range(1, n):
+            clause = [self.p(i, j) for j in range(i+1, n+1)]
+            formula.append(clause)
+
+        # every vertex (except n) is a parent to at most one vertex
+        for i in range(1, n):
+            for j in range(i+1, n+1):
+                for k in range(j+1, n+1):
+                    clause = [-self.p(i, j), -self.p(i, k)]
+                    formula.append(clause)
+
+        # sets 1c and 1d
+        for i in range(1, n+1):
+            for j in range(i+1, n+1):
+                for k in range(1, n+1):
+                    if self._edge_differs(i, j, k):
+                        clause = self.implies(
+                            # in the paper there is o(i, k), shouldn't there be o(j, k)?
+                            [self.p(i, j), self.o(i, k)],
+                            self.r(i, j, k)
+                        )
+                        formula.append(clause)
+
+        # TODO:
+        # semantics of a
+        # subset 1b
+        # subset 1a
+        # cardinality constraints
+
+
+        return formula
+
+    def _edge_differs(self, i, j, k):
+        return self.g.has_edge(i, k) ^ self.g.has_edge(j, k)
 
 
 def encode(g, tww):
