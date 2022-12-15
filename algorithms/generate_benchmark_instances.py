aea8ceb623c8ae4486ec9b0f0208a2b141ef220b
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-12-15 22:33:01 +0100

    Add benchmark graphs

--- algorithms/generate_benchmark_instances.py
+++ algorithms/generate_benchmark_instances.py
@@ -18,6 +18,25 @@ def random_edge_chance(n, chance):
     return g
 
 
+def random_planar(n):
+    while True:
+        g = random_edge_chance(n, 3/n)
+        if nx.is_planar(g):
+            return g
+
+def flatten(graph: nx.Graph):
+    index = 1
+    vertex_map = {}
+    for vertex in graph.nodes:
+        vertex_map[vertex] = index
+        index += 1
+
+    result = nx.Graph()
+    for u, v in graph.edges:
+        result.add_edge(vertex_map[u], vertex_map[v])
+    return result
+
+
 instances = [
     (nx.random_cograph(4), "cograph_16.gr"),
     (nx.random_cograph(5), "cograph_32.gr"),
@@ -37,12 +56,18 @@ instances = [
     (nx.star_graph(2**4), "star_16.gr"),
     (nx.star_graph(2**5), "star_32.gr"),
     (nx.star_graph(2**6), "star_64.gr"),
-    (nx.grid_2d_graph(4, 4), "grid_4x4.gr"),
-    (nx.grid_2d_graph(6, 6), "grid_6x6.gr"),
-    (nx.grid_2d_graph(8, 8), "grid_8x8.gr"),
+    (flatten(nx.grid_2d_graph(4, 4)), "grid_4x4.gr"),
+    (flatten(nx.grid_2d_graph(6, 6)), "grid_6x6.gr"),
+    (flatten(nx.grid_2d_graph(8, 8)), "grid_8x8.gr"),
+    (flatten(nx.hypercube_graph(4)), "hypercube_16.gr"),
+    (flatten(nx.hypercube_graph(5)), "hypercube_32.gr"),
+    (flatten(nx.hypercube_graph(6)), "hypercube_64.gr"),
     (random_edge_chance(16, 0.5), "edge_chance_0.5_16.gr"),
     (random_edge_chance(32, 0.5), "edge_chance_0.5_32.gr"),
     (random_edge_chance(64, 0.5), "edge_chance_0.5_64.gr"),
+    (random_planar(16), "planar_16.gr"),
+    (random_planar(32), "planar_32.gr"),
+    (random_planar(64), "planar_64.gr"),
 ]
 
 for graph, name in instances:
