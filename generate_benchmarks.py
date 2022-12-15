a70f764a00545f35e85f0c8952eacd2abaa7793a
Aleksander Katan <aleksander.katan@student.uj.edu.pl>
2022-12-16 00:53:23 +0100

    Exclude some benchmark graphs

--- generate_benchmarks.py
+++ generate_benchmarks.py
@@ -1,40 +1,17 @@
 import networkx as nx
 
-from algorithms.encodings.relative_sat_encoder import RelativeSatEncoder
-from algorithms.down_up_sat_checker import process
+# from algorithms.encodings.relative_sat_encoder import RelativeSatEncoder
+# from algorithms.down_up_sat_checker import process
+from ortools.linear_solver import pywraplp
+
+from algorithms.ilp_checker import process
 from algorithms.helpers import read_graph
 import os
 import time
-import multiprocessing.pool
-import functools
-
-
-def timeout(max_timeout):
-    """Timeout decorator, parameter in seconds."""
-
-    def timeout_decorator(item):
-        """Wrap the original function."""
-
-        @functools.wraps(item)
-        def func_wrapper(*args, **kwargs):
-            """Closure for function."""
-            pool = multiprocessing.pool.ThreadPool(processes=1)
-            async_result = pool.apply_async(item, args, kwargs)
-            # raises a TimeoutError if execution exceeds max_timeout
-            return async_result.get(max_timeout)
-
-        return func_wrapper
-
-    return timeout_decorator
-
+import func_timeout
 
 INSTANCES_PATH = 'benchmark_instances'
-TIMEOUT = 120
-
-
-@timeout(TIMEOUT)
-def run_algorithm(graph: nx.Graph):
-    return process(graph, RelativeSatEncoder(), [], "cadical")
+TIMEOUT = 2
 
 
 if __name__ == '__main__':
@@ -46,7 +23,9 @@ if __name__ == '__main__':
         print(f"{file_path}: ", end="")
         start_time = time.time()
         try:
-            tww, sequence = run_algorithm(graph)
+            tww, sequence = func_timeout.func_timeout(TIMEOUT,
+                                                      # lambda: process(graph, RelativeSatEncoder(), [], "cadical"))
+                                                      lambda: process(graph, pywraplp.Solver.CreateSolver("SCIP")))
             print(f"time: {time.time() - start_time}, twinwidth: {tww}")
-        except multiprocessing.context.TimeoutError:
+        except func_timeout.FunctionTimedOut:
             print(f"timeout (>{TIMEOUT})")
